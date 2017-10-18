03a69112a06a816f63ee122348b10452bccf5543
nkzawa <naoyuki.kanezawa@gmail.com>
2017-10-19 04:24:14 +0900

    fix the server URL

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,5 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "https://socketio-chat.now.sh/";
+    public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
 }
