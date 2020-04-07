ebe17741f33782f3fea0f7e7e3c9bd5dd4b0041c
Newton <newton@deligram.com>
2020-04-07 18:36:15 +0600

    Migrate to androidX

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/MainActivity.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/MainActivity.java
@@ -1,7 +1,7 @@
 package com.github.nkzawa.socketio.androidchat;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
